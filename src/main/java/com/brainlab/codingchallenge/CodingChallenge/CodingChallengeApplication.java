47c9ea4c95604c6eac4ddc1d235bb7571d7070e9
w.brahim@striped-giraffe.com <w.brahim@striped-giraffe.com>
2019-10-09 01:52:00 +0200

    update service and controller

--- src/main/java/com/brainlab/codingchallenge/CodingChallenge/CodingChallengeApplication.java
+++ src/main/java/com/brainlab/codingchallenge/CodingChallenge/CodingChallengeApplication.java
@@ -1,13 +1,20 @@
 package com.brainlab.codingchallenge.CodingChallenge;
 
+import java.lang.invoke.MethodHandles;
+
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class CodingChallengeApplication {
+    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
-	public static void main(String[] args) {
-		SpringApplication.run(CodingChallengeApplication.class, args);
-	}
+
+    public static void main(String[] args) {
+        LOGGER.info("\nWeb Service to add two ransom numbers started\n");
+        SpringApplication.run(CodingChallengeApplication.class, args);
+    }
 
 }
--- src/main/java/com/brainlab/codingchallenge/CodingChallenge/services/impl/CalculatorService.java
+++ src/main/java/com/brainlab/codingchallenge/CodingChallenge/services/impl/CalculatorService.java
@@ -2,12 +2,16 @@ package com.brainlab.codingchallenge.CodingChallenge.services.impl;
 
 import com.brainlab.codingchallenge.CodingChallenge.exceptions.InconvertibleToNumberException;
 import com.brainlab.codingchallenge.CodingChallenge.util.SumCalculator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.math.BigInteger;
 
 @Service
 public class CalculatorService {
+    private static final Logger LOG = LoggerFactory.getLogger(CalculatorService.class);
+
     private SumCalculator sumCalculator;
 
     /**
@@ -16,13 +20,20 @@ public class CalculatorService {
      * @param operandsString
      * @return
      */
-    public String calculateSum(String operandsString) throws InconvertibleToNumberException {
+    public String calculateSum(String operandsString) {
         sumCalculator = new SumCalculator();
 
-        BigInteger sum = sumCalculator.calculateFinalSum(operandsString);
+        String sum = null;
+        try {
+            sum = sumCalculator.calculateFinalSum(operandsString);
+
+        } catch (InconvertibleToNumberException e) {
+            LOG.error("Error in parsing input String : " + operandsString, e);
+            //e.printStackTrace();
+        }
+
         if (sum == null)
             return "{ \"sum\" : " + Double.NaN + "}";
         return "{ \"sum\" : " + sum + "}";
-
     }
 }
